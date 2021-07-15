b4af3e8487947f2516eaaa707d6b812d7f031a33
Evgenia Bazelan <evgeniabazelan@gmail.com>
2021-07-15 19:46:16 +0300

    profile

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,11 +1,23 @@
+// import React from 'react';
+// import { Route, Redirect } from "react-router-dom";
+//
+// const ProtectedRoute = ({component: Component, ...props}) => {
+//     return ( <Route>
+//             {() => props.loggedIn ?  <Component {...props} /> : <Redirect to="/sign-in" />} // допишите логику здесь
+//         </Route>
+//     )
+// }
+//
+// export default ProtectedRoute;
 import React from 'react';
 import { Route, Redirect } from "react-router-dom";
-
 const ProtectedRoute = ({component: Component, ...props}) => {
-    return ( <Route>
-            {() => props.loggedIn ?  <Component {...props} /> : <Redirect to="/sign-in" />} // допишите логику здесь
+    return (
+        <Route exact>
+            {
+                () => props.loggedIn ?  <Component {...props} /> : <Redirect to="/sign-in" />
+            }
         </Route>
     )
 }
-
 export default ProtectedRoute;
